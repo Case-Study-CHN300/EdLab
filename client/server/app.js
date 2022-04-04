c0a4c55f793a57116ac304b16e5381555b347f13
Vijay Golani <vijay_g@ch.iitr.ac.in>
2022-04-04 15:37:19 +0530

    pdf thing done

--- client/server/app.js
+++ client/server/app.js
@@ -1,12 +1,14 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const { default: mongoose } = require("mongoose");
+const cors=require('cors');
 const app = express();
+
 var cookieParser = require("cookie-parser");
 
 dotenv.config({ path: "./config.env" });
 require("./db/conn");
-
+app.use(cors());
 // json data jo bhi aaye usko simply dikha do
 app.use(express.json());
 
@@ -14,12 +16,16 @@ app.use(cookieParser());
 
 // link the router files
 app.use(require("./router/auth"));
-
+ 
 const PORT = process.env.PORT;
 
 app.get("/signin", (req, res) => {
   res.send("Hello world from the server");
 });
+
+app.get("/download",(req,res)=>{
+  res.download("Sand.pdf")
+})
 
 app.get("/signup", (req, res) => {
   res.send("Hello world from the server");
--- client/src/pages/research-safety.jsx
+++ client/src/pages/research-safety.jsx
@@ -5,7 +5,14 @@ import Question from '../components/Quiz/Question';
 import End from '../components/Quiz/Quiz1/End';
 import ScoreCard from '../components/Quiz/ScoreCard';
 import Modal from '../components/Quiz/Modal';
+<<<<<<< Updated upstream
 import quizData from '../components/Quiz/data/quiz1.json';
+=======
+import quizData from '../data/quiz.json';
+import Download from '../components/download/Download';
+import "react-awesome-slider/dist/styles.css";
+import "../components/Slider/slider.css";
+>>>>>>> Stashed changes
 import "../components/Quiz/quiz.css";
 import 'bulma/css/bulma.min.css';
 
@@ -88,6 +95,7 @@ const ResearchSafety = () => {
     };
   return (
     <>
+<<<<<<< Updated upstream
     <div className="Quiz_App">
       <RenderQuiz/>
           {step === 2 && <Question 
@@ -112,6 +120,38 @@ const ResearchSafety = () => {
             data={quizData.data}
           />}
     </div>   
+=======
+    <div className="App">
+    <div>
+        <Download />
+      </div>
+      {/* <h1>{userData.course1Marks}</h1> */}
+      {step === 1 && <Start onQuizStart={quizStartHandler}/>}
+      {step === 2 && <Question 
+        data={quizData.data[activeQuestion]}
+        onAnswerUpdate={setAnswers}
+        numberOfQuestions={quizData.data.length}
+        activeQuestion={activeQuestion}
+        onSetActiveQuestion={setActiveQuestion}
+        onSetStep={setStep}
+      />}
+      {step === 3 && <End 
+        results={answers}
+        data={quizData.data}
+        onReset={resetClickHandler}
+        onAnswersCheck={() => setShowModal(true)}
+        time={time}
+      />}
+
+      {showModal && <Modal 
+        onClose={() => setShowModal(false)}
+        results={answers}
+        data={quizData.data}
+      />}
+     
+    </div>
+   
+>>>>>>> Stashed changes
     </>
   )
 }
